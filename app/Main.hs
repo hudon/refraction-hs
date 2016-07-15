b2cbe0979a9d7a67d3fd4d0c7978acd880913a52
James Hudon <jameshudon@gmail.com>
2016-07-14 18:40:53 -0700

    add encodings to prompt

--- app/Main.hs
+++ app/Main.hs
@@ -8,10 +8,10 @@ import qualified Refraction as R
 
 main :: IO ()
 main = do
-    T.putStr "Enter source private key: "
+    T.putStr "Enter source private key (WIF encoded): "
     hFlush stdout
     prvkey <- T.getLine
-    putStr "Enter destination address: "
+    putStr "Enter destination address (Base58 encoded): "
     hFlush stdout
     pubkey <- T.getLine
     R.refract prvkey pubkey
