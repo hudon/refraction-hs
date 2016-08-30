513f147fd36fecc59f0964507c3e822d251589b5
James Hudon <hudon@uber.com>
2016-08-29 19:40:41 -0700

    add command line option for isBob

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,20 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import System.Console.GetOpt
+import System.Environment
 import System.IO
 import Control.Applicative
+import Data.Maybe (fromMaybe)
 import Data.Yaml
 import Data.Text (Text)
 import qualified Data.Text.IO as T
 import qualified Refraction as R
 
-data RefractionConfig = RefractionConfig {
-    bitcoin :: BitcoinConfig
-} deriving Show
-data BitcoinConfig = BitcoinConfig {
-    network :: Text
-} deriving Show
+
+-- Specify config file format
+data RefractionConfig = RefractionConfig { bitcoin :: BitcoinConfig } deriving Show
+data BitcoinConfig = BitcoinConfig { network :: Text } deriving Show
 instance FromJSON RefractionConfig where
     parseJSON (Object m) = RefractionConfig <$> m .: "bitcoin"
     parseJSON x = fail ("not an object: " ++ show x)
@@ -22,12 +23,30 @@ instance FromJSON BitcoinConfig where
     parseJSON x = fail ("not an object: " ++ show x)
 
 readConfig :: IO RefractionConfig
-readConfig =
-    either (error . show) id <$> decodeFileEither ".refraction.yaml"
+readConfig = either (error . show) id <$> decodeFileEither ".refraction.yaml"
 
-main :: IO ()
-main = do
-    let isBob = True
+-- Specify options
+data Options = Options  { optIsBob  :: Bool }
+defaultOptions :: Options
+defaultOptions = Options { optIsBob = False }
+options :: [OptDescr (Options -> IO Options)]
+options = [
+    Option ['b'] ["isbob"]   (NoArg showBob)   "run as Bob (server)"
+  ]
+showBob opt = do
+    putStrLn "Running Refraction as Bob (server)"
+    return opt { optIsBob = True }
+
+readOptions = do
+  args <- getArgs
+  let (actions, nonOpts, msgs) = getOpt RequireOrder options args
+  opts <- foldl (>>=) (return defaultOptions) actions
+  let Options { optIsBob = isBob } = opts
+  return isBob
+
+
+runRefraction :: Bool -> IO ()
+runRefraction isBob = do
     config <- readConfig
     T.putStr "Enter source private key (WIF encoded): "
     hFlush stdout
@@ -36,3 +55,7 @@ main = do
     hFlush stdout
     pubkey <- T.getLine
     R.refract (network (bitcoin config)) isBob prvkey pubkey
+
+main = do
+    isBob <- readOptions
+    runRefraction isBob
