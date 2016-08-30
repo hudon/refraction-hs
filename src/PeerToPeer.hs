e0c03db3f914d80d742e6e74be06492674978277
James Hudon <hudon@uber.com>
2016-08-29 23:31:12 -0700

    add the client to p2p module so that alice can send msg to bob

--- src/PeerToPeer.hs
+++ src/PeerToPeer.hs
@@ -13,11 +13,9 @@ runConn :: (Socket, SockAddr) -> Int -> IO ()
 runConn (sock, _) msgNum = do
     hdl <- socketToHandle sock ReadWriteMode
     hSetBuffering hdl NoBuffering
-
-    name <- liftM init (hGetLine hdl)
+    aliceHelloMsg <- liftM init (hGetLine hdl)
     hPutStrLn hdl "hi"
-    name <- liftM init (hGetLine hdl)
-
+    aliceByeMsg <- liftM init (hGetLine hdl)
     hClose hdl
 
 mainLoop :: Socket -> Int -> IO ()
@@ -33,5 +31,32 @@ startServer = do
     listen sock 2
     mainLoop sock 0
 
+
+runClient :: HostName             -- ^ Remote hostname, or localhost
+          -> String               -- ^ Port number or name; 4242 is default
+          -> IO ()
+runClient hostname port =
+    do -- Look up the hostname and port.  Either raises an exception
+       -- or returns a nonempty list.  First element in that list
+       -- is supposed to be the best option.
+       addrinfos <- getAddrInfo Nothing (Just hostname) (Just port)
+       let serveraddr = head addrinfos
+
+       -- Establish a socket for communication
+       sock <- socket (addrFamily serveraddr) Stream defaultProtocol
+
+       -- Connect to server
+       connect sock (addrAddress serveraddr)
+
+       -- Make a Handle out of it for convenience
+       h <- socketToHandle sock ReadWriteMode
+
+       hSetBuffering h NoBuffering
+
+       hPutStrLn h "hello"
+       bobHiMsg <- liftM init (hGetLine h)
+       hPutStrLn h "bye"
+       hClose h
+
 startClient :: IO ()
-startClient = undefined
+startClient = runClient "127.0.0.1" "4242"
