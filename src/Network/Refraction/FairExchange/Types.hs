3f6067304f937e93ae5b0f3a232fa75c87a9ed1d
James Hudon <jameshudon@gmail.com>
2016-10-22 16:23:07 -0700

    separate fair exchange

---
+++ src/Network/Refraction/FairExchange/Types.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+module Network.Refraction.FairExchange.Types
+    ( KeyPair
+    , Secret
+    , numSecrets
+    , numChallenges
+    , BobKeyMessage(..)
+    , AliceKeysMessage(..)
+    ) where
+
+import Data.Aeson (FromJSON, ToJSON)
+import Data.Text.Lazy (Text)
+import GHC.Generics
+import Network.Haskoin.Crypto
+
+type KeyPair = (PrvKey, PubKey)
+type Secret = Integer
+
+numSecrets = 20 :: Int
+numChallenges = 5 :: Int
+
+data BobKeyMessage = BobKeyMessage {
+      bKey1 :: PubKey
+    , bKey2 :: PubKey
+    , bHashes :: [Text] -- ^ text for the hex-encoded bytestring
+    , bSumHashes :: [Text] -- ^ text for the hex-encoded bytestring
+} deriving (Generic)
+
+instance ToJSON BobKeyMessage
+
+instance FromJSON BobKeyMessage
+
+data AliceKeysMessage = AliceKeysMessage {
+      aKey1 :: PubKey
+    , aKey2 :: PubKey
+    , aSecrets :: [Secret]
+} deriving (Generic)
+
+instance ToJSON AliceKeysMessage
+
+instance FromJSON AliceKeysMessage
